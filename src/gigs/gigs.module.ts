baa7de4b7dad3234b8e9db3c19cbc7ba438596df
urizennnn <igamerryt@gmail.com>
2025-06-17 16:18:05 +0100

    chore: updating branch

--- src/gigs/gigs.module.ts
+++ src/gigs/gigs.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GigsService } from './gigs.service';
 import { GigsController } from './gigs.controller';
+import { JWTService, SlugService } from 'libs';
 
 @Module({
   imports: [],
   controllers: [GigsController],
-  providers: [GigsService],
+  providers: [GigsService, SlugService, JWTService],
 })
 export class GigsModule {}
