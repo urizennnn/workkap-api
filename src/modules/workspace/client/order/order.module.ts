68ade588c20a9c1cf9ad1ed29bf548912a60e477
urizennnnnn <igamerryt@gmail.com>
2025-06-19 01:24:25 +0100

    fix order module providers

--- src/modules/workspace/client/order/order.module.ts
+++ src/modules/workspace/client/order/order.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { JWTService } from 'libs';
 
 @Module({
   imports: [],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, JWTService],
 })
 export class OrderModule {}
