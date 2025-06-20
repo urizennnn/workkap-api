c6425ae56823cb7a0815a4ab7faf372d87b13ee3
urizennnn <igamerryt@gmail.com>
2025-06-20 14:30:39 +0100

    chore: updating gig schema

--- src/modules/workspace/freelancer/gigs/dto/index.ts
+++ src/modules/workspace/freelancer/gigs/dto/index.ts
@@ -1,3 +1,4 @@
+import { PackageTier } from '@prisma/client';
 import * as v from 'valibot';
 
 export const GigSchema = v.object({
@@ -9,6 +10,58 @@ export const GigSchema = v.object({
   tags: v.optional(v.array(v.string())),
   description: v.string(),
   thirdPartyAgreement: v.optional(v.boolean()),
+  package: v.optional(
+    v.array(
+      v.object({
+        tier: v.pipe(v.enum(PackageTier, 'Invalid package tier')),
+        name: v.string(),
+        description: v.string(),
+        deliveryTime: v.number(),
+        customAssetDesign: v.optional(v.boolean()),
+        sourceFile: v.optional(v.boolean()),
+        contentUpload: v.optional(v.boolean()),
+        convertToHtmlCss: v.optional(v.boolean()),
+        revisions: v.optional(v.number()),
+        price: v.number(),
+        TotalPrice: v.optional(v.number()),
+      }),
+    ),
+  ),
+  extraServices: v.optional(
+    v.object({
+      deliveryTime: v.optional(v.number()),
+      extraPrice: v.optional(v.number()),
+    }),
+  ),
+  images: v.optional(
+    v.array(
+      v.object({
+        url: v.string(),
+        alt: v.optional(v.string()),
+      }),
+    ),
+  ),
+  video: v.optional(
+    v.object({
+      url: v.string(),
+      thumbnail: v.optional(v.string()),
+    }),
+  ),
+  documents: v.optional(
+    v.array(
+      v.object({
+        url: v.string(),
+        name: v.optional(v.string()),
+      }),
+    ),
+  ),
+  questions: v.optional(
+    v.array(
+      v.object({
+        text: v.string(),
+      }),
+    ),
+  ),
 });
 
 export type GigSchemaType = v.InferInput<typeof GigSchema>;
